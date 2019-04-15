c75b8061ec33f07cacf2eb5cb3c13e967c6bd322
LlGOuDaN <35547658+LlGOuDaN@users.noreply.github.com>
2019-04-14 20:55:19 -0400

    create basic UI

--- app/src/main/java/com/example/l8411/muternet/MainActivity.java
+++ app/src/main/java/com/example/l8411/muternet/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.l8411.muternet;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import jp.co.recruit_lifestyle.android.widget.PlayPauseButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PlayPauseButton playPauseButton = findViewById(R.id.play_pause_button);
+        playPauseButton.setColor(Color.WHITE);
     }
 }
